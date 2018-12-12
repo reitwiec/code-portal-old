c8be36c5b9c48738a9f0ddf492fba2fd05731cd7
Atika Tantiya <atika.tantiya@gmail.com>
2018-12-12 23:03:59 +0530

    modified contest routes

--- routes/admin.js
+++ routes/admin.js
@@ -5,23 +5,27 @@ const { question } = require('../models');
 module.exports = ()=>{
   let exp = {};
   exp.showcontests = async (req, res) =>{
-    contest.findAll().then(contests=>{
-		res.send(contests);
-	});
+    let err,contests;
+    if(req.user.access!=30) return res.sendError(null,'Access denied for user');
+    [err, contests] = await to(contest.findAll());
+    if(err) return res.sendError(err);
+    return res.sendSuccess(contests,'Successfully displaying contests');
   };
 
   exp.addcontest = async (req, res) =>{
-    const newcontest = {
-		id: req.body.id,
-        title: req.body.title,
-        start: req.body.start,
-        end: req.body.end,
-        description: req.body.description,
-        visibility: req.body.visibility,
-        slug: req.body.slug
-	};
-	contest.create(newcontest);
-	res.send(newcontest);
+    let newcontest,err;
+    if(req.user.access!=30) return res.sendError(null,'Access denied for user');
+    [err, newContest] = await to(contest.create({
+      id: req.body.id,
+      title: req.body.title,
+      start: req.body.start,
+      end: req.body.end,
+      description: req.body.description,
+      visibility: req.body.visibility,
+      slug: req.body.slug
+    }));	
+    if(err) return res.sendError(err);
+	  return res.sendSuccess(null,'Contest added successfully');
   };
 
   return exp;
