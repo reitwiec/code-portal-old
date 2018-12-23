fa2371bcb37d436b34a1e3318150bc81bc71a16b
Abhishek <abhi.aryan98@gmail.com>
2018-12-23 15:43:43 +0530

    cleaned code a bit

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,14 +1,18 @@
-const { submissions } = require('../models');
-const { user } = require('../models');
+const {
+    submissions
+} = require('../models');
+const {
+    user
+} = require('../models');
 const to = require('../utils/to');
 var sequelize = require('sequelize');
 const client = require('../utils/cache');
 
 module.exports = () => {
     let exp = {};
-    exp.showleaderboard = async(req, res) => {
+    exp.showleaderboard = async (req, res) => {
         client.zrangebyscore(req.params.contest, 1, 10000, 'LIMIT', 0, 10, (err, leaderboard) => {
-            if(err) {
+            if (err) {
                 console.log(err);
                 return res.sendError(err);
             }
@@ -17,103 +21,106 @@ module.exports = () => {
                 member = JSON.parse(member);
             });
 
-            
 
-            if(leaderboard.length == 0) {
+
+            if (leaderboard.length == 0) {
                 let LB = new Map();
                 let Leaderboard = [];
                 submissions.findAll({
-                    where: { contest: req.params.contest },
-                    raw: true,
-                    attributes: {
-                        include: [
-                            sequelize.fn('MAX', sequelize.col('points')),
-                        ],
-                        exclude: []
-                    },
-                    group: ['user', 'question']
-				})
-				.then((users) => {
-                    // Iterate in users and calculate total score for each user
-                    users.forEach((item, index) => {
-                        if (LB.has(item.user)){
-                            let temp = LB.get(item.user);
-                            LB.set(item.user, temp + item.points);
-                        }
-                        else {
-                            LB.set(item.user, item.points);
-                        }
-                    // console.log(item.user + ' ' + item.points);
-                    });
-				})
-				.then(() => {
-                    // console.log(LB);
-                	var promises = [];
-                    LB.forEach((value, key, map) => {
-                        // Find Username from User Table
-                        // Query returns a promise that we push to an array and resolve later
-                        promises.push(user.findOne({where: {id: key}}));
-                    });
-
-                    if(LB.length == 0) {
-                        return res.sendSuccess(null, 'No submissions have been made yet');
-                    }
-
-                    Promise.all(promises).then((users) => {
-                        for (var i in users) {
-                            Leaderboard.push({
-                                userid: users[i].id,
-                                username: users[i].name,
-                                points: LB.get(users[i].id)
-                            });
-                        }
-                        
-                        // Sort the leaderboard
-                		Leaderboard.sort((a, b) => {
-                        	// Compare Function
-                        	if (a.points > b.points) {
-                            	return -1;
-                        	}
-                    		if (a.points < b.points) {
-                            	return 1;
-                        	}
-                        	return 0;
-                    	});
-                        
-                        // Assigning Rank
-                        var Rank = 1;
-                        Leaderboard[0].rank = Rank;
-                        for (var i = 1; i < Leaderboard.length; i++) {
-                            if (Leaderboard[i].points != Leaderboard[i-1].points)
-                                Rank++;
-                            Leaderboard[i].rank = Rank;
-                        }
-                        // console.log(Leaderboard);
-                        if(Leaderboard.length != 0) {
-                            Leaderboard.forEach((member) => {
-                                client.zadd(req.params.contest, member.rank, JSON.stringify(member), (err, response) => { 
-                                    if (err) console.log(err);
-                                });
-                            });
-                            client.expire(req.params.contest, 30);
-                            client.zrangebyscore(req.params.contest, 0, 10000, 'LIMIT', 0, 10, (err, leaderboard) => {
-                                if(err) {
-                                    console.log(err);
-                                    return res.sendError(err);
+                        where: {
+                            contest: req.params.contest
+                        },
+                        raw: true,
+                        attributes: {
+                            include: [
+                                sequelize.fn('MAX', sequelize.col('points')),
+                            ],
+                            exclude: []
+                        },
+                        group: ['user', 'question']
+                    })
+                    .then((users) => {
+                        // Iterate in users and calculate total score for each user
+                        users.forEach((item, index) => {
+                            if (LB.has(item.user)) {
+                                let temp = LB.get(item.user);
+                                LB.set(item.user, temp + item.points);
+                            } else {
+                                LB.set(item.user, item.points);
+                            }
+                            // console.log(item.user + ' ' + item.points);
+                        });
+                    })
+                    .then(() => {
+                        // console.log(LB);
+                        var promises = [];
+                        LB.forEach((value, key, map) => {
+                            // Find Username from User Table
+                            // Query returns a promise that we push to an array and resolve later
+                            promises.push(user.findOne({
+                                where: {
+                                    id: key
                                 }
-        
-                                return res.sendSuccess(leaderboard);
-                            });
-                        }
-					})
-                    .catch(err => console.log(err));
-                    
-				})
-                .catch(err => console.log(err));
+                            }));
+                        });
 
-            } 
-            
-            else
+                        if (LB.length == 0) {
+                            return res.sendSuccess(null, 'No submissions have been made yet');
+                        }
+
+                        Promise.all(promises).then((users) => {
+                                for (var i in users) {
+                                    Leaderboard.push({
+                                        userid: users[i].id,
+                                        username: users[i].name,
+                                        points: LB.get(users[i].id)
+                                    });
+                                }
+
+                                // Sort the leaderboard
+                                Leaderboard.sort((a, b) => {
+                                    // Compare Function
+                                    if (a.points > b.points) {
+                                        return -1;
+                                    }
+                                    if (a.points < b.points) {
+                                        return 1;
+                                    }
+                                    return 0;
+                                });
+
+                                // Assigning Rank
+                                var Rank = 1;
+                                Leaderboard[0].rank = Rank;
+                                for (var i = 1; i < Leaderboard.length; i++) {
+                                    if (Leaderboard[i].points != Leaderboard[i - 1].points)
+                                        Rank++;
+                                    Leaderboard[i].rank = Rank;
+                                }
+                                // console.log(Leaderboard);
+                                if (Leaderboard.length != 0) {
+                                    Leaderboard.forEach((member) => {
+                                        client.zadd(req.params.contest, member.rank, JSON.stringify(member), (err, response) => {
+                                            if (err) console.log(err);
+                                        });
+                                    });
+                                    client.expire(req.params.contest, 30);
+                                    client.zrangebyscore(req.params.contest, 0, 10000, 'LIMIT', 0, 10, (err, leaderboard) => {
+                                        if (err) {
+                                            console.log(err);
+                                            return res.sendError(err);
+                                        }
+
+                                        return res.sendSuccess(leaderboard);
+                                    });
+                                }
+                            })
+                            .catch(err => console.log(err));
+
+                    })
+                    .catch(err => console.log(err));
+
+            } else
                 return res.sendSuccess(leaderboard);
         });
     }
