e1c2409457705fd15b3e3d925e2a1999e98cd71c
pranavtharoor <pranav.tharoor@gmail.com>
2019-03-28 17:56:54 +0530

    Added try catch to submission request

--- routes/submissions.js
+++ routes/submissions.js
@@ -84,19 +84,25 @@ module.exports = io => {
             let output = await read_file_promise(
               casesJSON[subcase.testcase_id].output_path
             );
-            let result = await axios.post(
-              process.env.JUDGE_API,
-              {
-                source_code: req.body.source,
-                language_id: lang.code,
-                stdin: input,
-                expected_output: output,
-                cpu_time_limit: lang.multiplier * ques.time_limit
-              },
-              {
-                maxContentLength: 52428890
-              }
-            );
+            let result;
+            try {
+              result = await axios.post(
+                process.env.JUDGE_API,
+                {
+                  source_code: req.body.source,
+                  language_id: lang.code,
+                  stdin: input,
+                  expected_output: output,
+                  cpu_time_limit: lang.multiplier * ques.time_limit
+                },
+                {
+                  maxContentLength: 52428890
+                }
+              );
+            } catch (err) {
+              console.log('axios error');
+              return res.sendError();
+            }
             // console.log(result);
             let status = result.data.status.id;
             let verdict = 'PROC';
